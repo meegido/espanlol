5ca146a53edc3fe363e5478e40b9a298d4f09c5c
Mercedes Egido <mercedes.egido@creditas.com>
2021-06-03 17:59:44 +0200

    button

--- src/modules/hero/hero.component.style.js
+++ src/modules/hero/hero.component.style.js
@@ -5,18 +5,29 @@ const heroSize = () => css`
   background-image: url(https://res.cloudinary.com/dekysavu4/image/upload/v1622728615/Espa%C3%B1lol/JPG%20rgb%2072%20dpi%20%28para%20web%29/hero-sized_uzn9ql.png);
   background-size: contain;
   background-repeat: no-repeat;
-  width: 130vw;
+  width: 120vw;
   height: 50vh;
-  position: absolute;
-  left: -50px;
+  transform: translate(-80px, 0px);
 
 
-  ${props => config(props).media['sm']`
-    background-repeat: repeat-x;
-    transform: translateY(-100px);
+  ${props => config(props).media['md']`
+    background-repeat: no-repeat;
+    transform: translate(180px, -100px);
     z-index: -1;
-    background-size: 35%;
-    height: 100vh;
+    background-size: 50%;
+    height: 70vh;
+  `}
+
+  ${props => config(props).media['lg']`
+    transform: translate(380px, -120px);
+    background-size: 36%;
+  
+  `}
+
+  ${props => config(props).media['xl']`
+    transform: translate(500px, -180px);
+    background-size: 33%;
+    height: 70vh;
   `}
 `
 
@@ -25,17 +36,62 @@ export const HeroImage = styled.div`
 `
 
 const titleSize = () => css`
-  position: absolute;
-  top: 70vh;
-  width: 100vw;
+  margin-top: 10em;
+  padding-right: 1.5em;
 
-  ${props => config(props).media['sm']`
-    top: 70vh;
-    width: 30vw;
-    left: 35vw;
+  ${props => config(props).media['md']`
+    margin: 0 auto;
+    padding-left: 6em;
+    width: 50vw;
 `}
+
+  ${props => config(props).media['lg']`
+    padding-left: 5em;
+    width: 30vw;
+  `}
 `
 
 export const HeroTitle = styled.img`
   ${titleSize}
+`
+
+const buttonPosition = () => css`
+  margin-top: 1em;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  align-items: end;
+`
+
+const buttonSize = () => css`
+  text-transform: uppercase;
+  font-family: 'Tampa';
+  font-size: 1.3em;
+  letter-spacing: 0.03em;
+  padding: 10px 56px 10px 21px;
+  background: ${props => props.theme.colors.black};
+  box-shadow: 0px 4px 8px -1px rgba(0, 0, 0, 0.25);
+  border-radius: 20px;
+  color: ${props => props.theme.colors.white};
+
+  &:before {
+    content: '';
+    position: absolute;
+    background-image: url("https://res.cloudinary.com/dekysavu4/image/upload/v1622735298/Espa%C3%B1lol/money-icon_ycqgeq.png");
+    width: 33px;
+    height: 33px;
+    background-repeat: no-repeat;
+    background-size: cover;
+    left: 57%;
+    top: 92.5%;
+  }
+
+`
+export const ButtonContainer = styled.div`
+  ${buttonPosition}
+`
+
+export const BuyButton = styled.button`
+ 
+  ${buttonSize}
 `

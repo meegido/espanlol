e2648cae020819182e844e1eb39089385afd9398
Mercedes Egido <mercedes.egido@creditas.com>
2021-06-03 15:33:31 +0200

    reverse boolean

--- src/modules/nav/nav.container.style.js
+++ src/modules/nav/nav.container.style.js
@@ -94,16 +94,16 @@ export const Burguer = styled.div`
     transition: all 0.3s linear;
 
     &:nth-child(1) {
-      transform: ${({open}) => open ? 'rotate(45deg)' : 'rotate(0)'};
+      transform: ${({open}) => open ? 'rotate(0)' : 'rotate(45deg)'};
     }
 
     &:nth-child(2) {
-      transform: ${({open}) => open ? 'translateX(100%)' : 'translateX(0)'};
-      opacity: ${({open}) => open ? 0 : 1};
+      transform: ${({open}) => open ? 'translateX(0)' : 'translateX(100%)'};
+      opacity: ${({open}) => open ? 1 : 0};
     }
 
     &:nth-child(3) {
-      transform: ${({open}) => open ? 'rotate(-45deg)' : 'rotate(0)'}
+      transform: ${({open}) => open ? 'rotate(0)' : 'rotate(-45deg)'}
     }
   }
 `

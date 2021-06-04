a693b0aba49272d0b769fe876bbdda02200dc852
Mercedes Egido <mercedes.egido@creditas.com>
2021-06-04 20:41:53 +0200

    disc section

--- src/modules/disc/disc.container.js
+++ src/modules/disc/disc.container.js
@@ -1,21 +1,73 @@
 import React from 'react'
-import { Col, Row } from 'react-awesome-styled-grid'
-import { Title, ProfileImage, SocialContainer, SocialIcon } from './disc.container.style'
+import {Col, Row} from 'react-awesome-styled-grid'
+import {
+  TitleProfile,
+  ProfileImage,
+  SocialContainer,
+  SocialIcon,
+  SectionTitle,
+  ReadButton,
+  SocialWithButtonContainer
+} from './disc.container.style'
 
 export const DiscContainer = () => {
   return (
-    <Col xs={4} md={8} lg={12} style={{padding: 0, margin: 0}} >
-      <Title>Carlos Gárate</Title>
-      <Row justify='center'>
+    <Col
+      xs={4}
+      md={8}
+      lg={12}
+      style={{padding: 0, margin: 0, borderBottom: '1px solid black'}}
+    >
+      <TitleProfile>Carlos Gárate</TitleProfile>
+      <Row justify="center">
         <Col xs={4} md={4} lg={6} style={{padding: '0 2em'}}>
-          <ProfileImage alt="Carlos Gárate" src="https://res.cloudinary.com/dekysavu4/image/upload/v1622818575/Espa%C3%B1lol/carlosgarate_awn0si.jpg" />
-          <p>Carlos Gárate Marquerie es un artista sonoro y músico que vive y trabaja en Madrid. Completó sus estudios de arte sonoro en la Universidad de Artes de Londres. Carlos ha trabajado en torno a la agencia de la materialidad de la tierra y sus energías en los sistemas sociales.</p>
-          <p>Además se dedica a componer música para danza y teatro trabajando junto a Elena Córdoba, el colectivo la imperfecta o participando en la composición musical y en la gira de Grito Pelao junto a Silvia Perez Cruz y Rocío Molina. Esta última le ha llevado a tocar en algunos de los teatros más prestigiosos de España y Francia como el Théâtre National de Chaillot en París, Los teatros del Canal en Madrid, La Maestranza en Sevilla o el Teatre Grec en Barcelona.</p>
+          <ProfileImage
+            alt="Carlos Gárate"
+            src="https://res.cloudinary.com/dekysavu4/image/upload/v1622818575/Espa%C3%B1lol/carlosgarate_awn0si.jpg"
+          />
+          <p>
+            Carlos Gárate Marquerie es un artista sonoro y músico que vive y
+            trabaja en Madrid. Completó sus estudios de arte sonoro en la
+            Universidad de Artes de Londres. Carlos ha trabajado en torno a la
+            agencia de la materialidad de la tierra y sus energías en los
+            sistemas sociales.
+          </p>
+          <p>
+            Además se dedica a componer música para danza y teatro trabajando
+            junto a Elena Córdoba, el colectivo la imperfecta o participando en
+            la composición musical y en la gira de Grito Pelao junto a Silvia
+            Perez Cruz y Rocío Molina. Esta última le ha llevado a tocar en
+            algunos de los teatros más prestigiosos de España y Francia como el
+            Théâtre National de Chaillot en París, Los teatros del Canal en
+            Madrid, La Maestranza en Sevilla o el Teatre Grec en Barcelona.
+          </p>
           <SocialContainer>
-            <SocialIcon alt="Instagram profile" src="https://res.cloudinary.com/dekysavu4/image/upload/v1622816831/Espa%C3%B1lol/iconos/logo-computer-icons-clip-art-instagram-layout-238806f88991bbd95457aa43ae40ca85_yotwzh.png"/>
-            <SocialIcon alt="Soundcloud profile" src="https://res.cloudinary.com/dekysavu4/image/upload/v1622816760/Espa%C3%B1lol/iconos/580b57fcd9996e24bc43c537_xr3vsl.png"/>
+            <SocialIcon
+              alt="Instagram profile"
+              src="https://res.cloudinary.com/dekysavu4/image/upload/v1622816831/Espa%C3%B1lol/iconos/logo-computer-icons-clip-art-instagram-layout-238806f88991bbd95457aa43ae40ca85_yotwzh.png"
+            />
+            <SocialIcon
+              alt="Soundcloud profile"
+              src="https://res.cloudinary.com/dekysavu4/image/upload/v1622816760/Espa%C3%B1lol/iconos/580b57fcd9996e24bc43c537_xr3vsl.png"
+            />
           </SocialContainer>
         </Col>
+      </Row>
+      <SectionTitle>Disco</SectionTitle>
+      <Row justify="center">
+        <Col xs={4} md={4} lg={6} style={{padding: '0 2em'}}>
+        <ProfileImage
+            alt="Cover Españlol"
+            src="https://res.cloudinary.com/dekysavu4/image/upload/v1622632727/Espa%C3%B1lol/JPG%20rgb%2072%20dpi%20compressed%20%28para%20email%20y%20webs%20donde%20os%20pidan%20que%20pesen%20poco%29/PORTADA-ESPA%C3%91LOL-OK-2-72_dpi_3000x3000_ad5ibc.jpg"
+          />
+        </Col>
+        <SocialWithButtonContainer style={{marginTop: '2em'}}>
+            <SocialIcon
+              alt="Read more"
+              src="https://res.cloudinary.com/dekysavu4/image/upload/v1622816589/Espa%C3%B1lol/iconos/logo-spotify-icon-4096_d6hdlj.png"
+            />
+            <ReadButton>Leer mas</ReadButton>
+          </SocialWithButtonContainer>
       </Row>
     </Col>
   )
--- src/modules/disc/disc.container.style.js
+++ src/modules/disc/disc.container.style.js
@@ -4,17 +4,18 @@ import {config} from 'react-awesome-styled-grid'
 const titleStyle = () => css`
   border-top: 1px solid ${props => props.theme.colors.black};
   border-bottom: 1px solid ${props => props.theme.colors.black};
-  font-family: 'Trueno', sans-serif;
   text-align: center;
-  font-size: 24px;
-  text-transform: uppercase;
   letter-spacing: 0.01em;
-  line-height: 114.1%;
+  line-height: 32px;
   margin-bottom: 1em;
+  padding: .3em;
 `
 
-export const Title = styled.h2`
+export const TitleProfile = styled.h2`
   ${titleStyle}
+  font-family: 'Trueno', sans-serif;
+  font-size: 24px;
+  text-transform: uppercase;
 `
 
 const profleStyle = () => css`
@@ -28,19 +29,45 @@ export const ProfileImage = styled.img`
 const socialContainer = () => css`
   display: flex;
   justify-content: center;
-  align-items: baseline;
   margin-bottom: 2em;
+  
 `
 
 export const SocialContainer = styled.div`
   ${socialContainer}
+  align-items: baseline;
 `
 
 export const SocialIcon = styled.img`
-  width: 20%;
+  width: 15%;
   padding: 0 1em;
 
   ${props => config(props).media['sm']`
     width: 8%;
   `}
+`
+
+export const SectionTitle = styled.h2`
+  ${titleStyle}
+  font-family: 'Tampa', sans-serif;
+  font-size: 40px;
+  margin-bottom: .5em;
+`
+
+export const SocialWithButtonContainer = styled.div`
+  ${socialContainer}
+  align-items: center;
+`
+
+const readButton = () => css`
+  background: transparent;
+  border-radius: 20px;
+  border: 1px solid ${props => props.theme.colors.black};
+  padding: .5em 1em;
+  font-family: 'Tampa', sans-serif;
+  font-size: 10px;
+`
+
+export const ReadButton = styled.button`
+  ${readButton}
 `

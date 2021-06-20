311d8244d990bc6c040480e9a945de7e6991d640
Mercedes Egido <mercedes.egido@creditas.com>
2021-06-20 17:44:42 +0200

    fixed gif images on safari

--- src/modules/cajas/cajas.container.js
+++ src/modules/cajas/cajas.container.js
@@ -3,7 +3,6 @@ import {Col, Row} from 'react-awesome-styled-grid'
 
 export const CajasContainer = () => {
   return ( 
-
     <Row justify="center">
     <Col
       debug
@@ -12,7 +11,7 @@ export const CajasContainer = () => {
       lg={12}
       style={{padding: 0, margin: 0}}
     >
-      <img alt="Cajas destemplás single reel" src="https://res.cloudinary.com/dekysavu4/image/upload/v1622882625/Espa%C3%B1lol/gifs/gif-cajas-244_sfitzq.gif" />
+      <img style={{padding: 0, width: "100%"}} alt="Cajas destemplás single reel" src="https://res.cloudinary.com/dekysavu4/image/upload/v1622882625/Espa%C3%B1lol/gifs/gif-cajas-244_sfitzq.gif" />
     </Col>
     </Row>
   )
--- src/modules/oriental/oriental.container.js
+++ src/modules/oriental/oriental.container.js
@@ -10,7 +10,7 @@ export const OrientalContainer = () => {
         lg={12}
         style={{padding: 0, margin: 0}}
       >        
-        <img alt="Oriental reel single" src="https://res.cloudinary.com/dekysavu4/image/upload/v1622883469/Espa%C3%B1lol/gifs/oriental-33_blpykd.gif" />
+        <img style={{padding: 0, width: "100%"}} alt="Oriental reel single" src="https://res.cloudinary.com/dekysavu4/image/upload/v1622883469/Espa%C3%B1lol/gifs/oriental-33_blpykd.gif" />
       </Col>
     </Row>
   )
